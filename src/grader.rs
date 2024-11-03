7dd1b3b5e6b2ce7416c19a56313b6217c8f4f380
G36maid <miku65434@gmail.com>
2024-11-04 00:51:30 +0800

    fix: testcase munber

--- src/grader.rs
+++ src/grader.rs
@@ -78,7 +78,7 @@ pub fn grade_student(
             println!("./testcase/{}/out/{}.out", testcase_name, i);
 
             let diff_command = format!(
-                "colordiff -s -Z -b {}/{}.out ./testcase/{}/out/{}.out",
+                "colordiff -s -u -Z -b {}/{}.out ./testcase/{}/out/{}.out",
                 student_output_folder, i, testcase_name, i
             );
             //println!("diff_command: {}", diff_command);
